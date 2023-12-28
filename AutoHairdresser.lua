78f1721b883d426802add429a8f6c54a6569a895
Richy Z <64844585+Richy-Z@users.noreply.github.com>
2023-12-28 23:48:10 +0000

    Given appropriate name and modified to adhere to Numelon Script Specification
    Given the script a new coherent name, and completely rewritten the script so that it adheres to Numelon's Lua Code Specification.

---
+++ AutoHairdresser.lua
@@ -0,0 +1,81 @@
+--[[
+    Revision Unknown
+    Author iirzd
+    Numelon Softworks
+
+    Updated Thursday 28th December 2023 at 23:19 GMT
+]]
+--[[
+    Latest update:
+    - Rewritten the script fully to align with Numelon's Code Specification
+    - Variable Names align with NC Spec
+    - Function Definitions align with NC Spec
+    - etc.
+]]
+local PlayerService = game:GetService("Players")
+
+local remote
+local orderNumber = 0
+local workstations = {}
+
+local function getOrder(workstation)
+    local orderValue = workstation.Occupied.Value:WaitForChild("Order")
+
+    return {
+        orderValue.Style.Value,
+        orderValue.Color.Value
+    }
+end
+
+local function doWork(workstation)
+    local workEvent
+    local inUseEvent
+    
+    workstations[workstation] = true
+
+    workEvent = workstation.Occupied.Changed:Connect(function(value)
+        if not value then return end
+
+        remote:FireServer({
+            Type = "FinishHair",
+            Workstation = workstation,
+            Order = getOrder(workstation)
+        })
+
+        orderNumber = orderNumber + 1
+        local date   = os.date("*t", now)
+        print("Finished order ".. orderNumber .." at ".. date["hour"] ..":".. date["min"])
+    end)
+
+    inUseEvent = workstation.InUse.Changed:Connect(function(value)
+        if value ~= PlayerService.LocalPlayer and workEvent ~= nil then
+            warn("You have changed your workstation. Stopping the old workEvent and resetting metamethod.")
+            workstations = {}
+            workEvent:Disconnect()
+            inUseEvent:Disconnect()
+        end
+    end)
+end
+
+-- Actual Processing --
+warn("Setting up metamethods")
+
+local metatable = getrawmetatable(game)
+local backupNamecall = metatable.__namecall
+
+-- make the metatable writeable
+local setreadonly = make_writeable or setreadonly -- some executors have different function names so we have to check if they exist
+setreadonly(metatable, false)
+
+-- overwrite namecall metamethod
+metatable.__namecall = function(...)
+    local args = {...}
+    if args[#args] == "FireServer" then
+        if args[2] and type(args[2]) == "table" and args[2]["Order"] and not workstations[args[2]["Workstation"]] then
+            warn("The remote and the workstation have been found!")
+            remote = args[1]
+            doWork(args[2]["Workstation"])
+        end
+    end
+    return backupNamecall(...)
+end

020d9288a06b4f113a3f7fa5b08b2deee9088c6f
Richard Ziupsnys <64844585+Richy-Z@users.noreply.github.com>
2025-07-20 12:16:14 -0700

    Update AntiIdle.lua

--- AntiIdle.lua
+++ AntiIdle.lua
@@ -1,16 +1,8 @@
 --[[
     Revision Unknown
     Author iirzd
-    Numelon Softworks
 
     Updated Thursday 29th December 2023 at 00:13 GMT
-]]
---[[
-    Latest update:
-    - Rewritten the script fully to align with Numelon's Code Specification
-    - Variable Names align with NC Spec
-    - Function Definitions align with NC Spec
-    - etc.
 ]]
 local VirtualUserService = game:GetService("VirtualUser")
 local PlayerService = game:GetService("Players")
